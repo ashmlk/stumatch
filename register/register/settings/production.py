bb1e7dce01f9e588e24b94e3ae80416a240a1891
ashmlk <ARSHIA@apples-MacBook-Pro-2.local>
2020-09-07 00:06:36 -0400

    "production"

--- register/register/settings/production.py
+++ register/register/settings/production.py
@@ -5,7 +5,7 @@ import django_heroku
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['corscope.heroku.com']
+ALLOWED_HOSTS = ['corscope.heroku.com','wwww.corscope.com','corscope.com']
 
 #EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 #EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
@@ -14,7 +14,7 @@ redis_url = urlparse(os.environ.get('REDISTOGO_URL', 'redis://localhost:6959'))
 
 CACHES = {
     'default': {
-        'BACKEND': 'redis_cache.RedisCache',
+        'BACKEND': "django_redis.cache.RedisCache",
         'LOCATION': '%s:%s' % (redis_url.hostname, redis_url.port),
         'OPTIONS': {
             'DB': 0,
