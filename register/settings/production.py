afacde29f64a97b1178eb4db30c4844405cf48a2
ashmlk <ARSHIA@apples-MacBook-Pro-2.local>
2020-09-07 12:41:14 -0400

    updated settings

--- register/settings/production.py
+++ register/settings/production.py
@@ -7,6 +7,39 @@ import dj_database_url
 
 DEBUG = False
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
+
 ALLOWED_HOSTS = ['corscope.heroku.com','wwww.corscope.com','corscope.com']
 
 #EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
@@ -31,9 +64,13 @@ CELERY_BROKER_TRANSPORT_OPTIONS = {
     "max_connections": 2,
 }
 
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 django_heroku.settings(locals(),logging=False)
