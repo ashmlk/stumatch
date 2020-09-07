e13ab46b0afddd3015dfc3fdf6c98b4cb9f8ae67
ashmlk <ARSHIA@apples-MacBook-Pro-2.local>
2020-09-07 00:05:41 -0400

    "heroku initial"

---
+++ register/register/settings/production.py
@@ -0,0 +1,34 @@
+from register.settings.common import *
+import os
+from urllib.parse import urlparse
+import django_heroku
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['corscope.heroku.com']
+
+#EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+#EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+redis_url = urlparse(os.environ.get('REDISTOGO_URL', 'redis://localhost:6959'))
+
+CACHES = {
+    'default': {
+        'BACKEND': 'redis_cache.RedisCache',
+        'LOCATION': '%s:%s' % (redis_url.hostname, redis_url.port),
+        'OPTIONS': {
+            'DB': 0,
+            'PASSWORD': redis_url.password,
+        }
+    }
+}
+
+CELERY_BROKER_URL=os.environ.get('REDISTOGO_URL')
+CELERY_RESULT_BACKEND=os.environ.get('REDISTOGO_URL')
+CELERY_BROKER_TRANSPORT_OPTIONS = {
+    "max_connections": 2,
+}
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
