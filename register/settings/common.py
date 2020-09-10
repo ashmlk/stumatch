7c9f0471758f55dffadbee38b7ba94e6d2269677
ashmlk <ARSHIA@apples-MacBook-Pro-2.local>
2020-09-10 10:33:13 -0400

    added www redirect

--- register/settings/common.py
+++ register/settings/common.py
@@ -183,6 +183,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'main.middleware.WwwRedirectMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
