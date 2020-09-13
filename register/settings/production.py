298284ce7d1fb457adfc81c87e997e226e72a4de
ashmlk <ARSHIA@apples-MacBook-Pro-2.local>
2020-09-13 13:08:49 -0400

    settings changed

--- register/settings/production.py
+++ register/settings/production.py
@@ -88,16 +88,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
 AWS_DEFAULT_ACL = None
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-
-MEDIA_LOCATION = "media"
-MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/"
-
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400',}
 DEFAULT_FILE_STORAGE = 'register.storage_backends.MediaStorage'
 
 
