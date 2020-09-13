2a88c625a92edf7b0a4e668c821da361555e2243
ashmlk <ARSHIA@apples-MacBook-Pro-2.local>
2020-09-13 09:45:23 -0400

    settings changed

--- register/settings/production.py
+++ register/settings/production.py
@@ -88,8 +88,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-PUBLIC_MEDIA_LOCATION = 'media'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
