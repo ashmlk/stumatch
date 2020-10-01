1f187698e32fd6924b83dd7bb826190ac8bc3e57
Arshia Malek <arshiamalek@Arshias-MacBook-Pro.local>
2020-09-30 20:39:09 -0400

    "all"

--- static/js/courses/course-instructor.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $(document).on('click', '.course-title-box', function (e) {
-        e.stopImmediatePropagation();
-        var url = $(this).attr("data-url")
-        document.location.href = url 
-    });
-})
