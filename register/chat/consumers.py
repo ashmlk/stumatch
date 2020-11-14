9a847cc0cae312d4e4f6fe05da5c3648b2c78d50
Arshia Malek <arshiamalek@Arshias-MacBook-Pro.local>
2020-11-13 23:58:47 -0500

    "part 2 done"

--- register/chat/consumers.py
+++ register/chat/consumers.py
@@ -1,45 +1,85 @@
+from asgiref.sync import async_to_sync
 import json
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer
+from .models import Message
+from main.models import Profile
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
+class ChatConsumer(WebsocketConsumer):
+        
+    def fetch_messages(self, data):
+        messages = Message.last_10_messages()
+        result = []
+        for message in messages:
+            json_message =  {
+            'author':message.author.username,
+            'content':message.content,
+            'timestamp':str(message.timestamp)
+            }
+            result.append(json_message)
+        content = {
+            'command':'messages',
+            'messages': result
+        }
+        self.send_chat_message(content)
+        
+    def new_message(self, data):
+        author = data['from']
+        author_user = Profile.objects.get(username=author)
+        print(data['message'])
+        message = Message.objects.create(
+            author=author_user,
+            content=data['message'])
+        json_message =  {
+            'author':message.author.username,
+            'content':message.content,
+            'timestamp':str(message.timestamp)
+            }
+        content = {
+            'command':'new_message',
+            'message':json_message
+        }
+        return self.send_chat_message(content)
+    
+    commands = {
+        'fetch_messages':fetch_messages,
+        'new_message':new_message
+    }
+    
+    def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
-        # Join room group
-        await self.channel_layer.group_add(
+        async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
 
-        await self.accept()
+        self.accept()
 
-    async def disconnect(self, close_code):
-        # Leave room group
-        await self.channel_layer.group_discard(
+    def disconnect(self, close_code):
+
+        async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
 
-    # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        # Send message to room group
-        await self.channel_layer.group_send(
+    def receive(self, text_data):
+        data = json.loads(text_data)
+        self.commands[data['command']](self, data)
+        
+    
+    def send_chat_message(self, message):
+        async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
                 'message': message
             }
         )
-
-    # Receive message from room group
-    async def chat_message(self, event):
+    def send_message(self, message):
+        self.send(text_data=json.dumps(message))
+        
+    def chat_message(self, event):
         message = event['message']
-
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+        self.send(text_data=json.dumps(message))
+        
--- register/chat/models.py
+++ register/chat/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from main.models import Profile
 
-# Create your models here.
+class Message(models.Model):
+    author = models.ForeignKey(Profile, related_name='author_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.author.username
+    
+    def last_10_messages():
+        return Message.objects.order_by('timestamp').all()[:10]
